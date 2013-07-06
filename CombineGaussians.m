d5f1c78415bcaaf9f436d19670827462a58af1ca
Dávid <triszalo@ubi.(none)>
2013-07-06 13:24:14 +0200

    CombineGaussians: simpler, well structured

--- CombineGaussians.m
+++ CombineGaussians.m
@@ -1,24 +1,19 @@
 function out = CombineGaussians(in)
 
-%in: 1xn struct array containing the fields: Ci, mu
+%in: nx1 struct array containing the fields: Ci, mu
 %out: struct containing the fields: C, Ci, mu
 
+%Ci3 = Ci1 + Ci2
+%C3 = inv(Ci3)
+%mu3 = C3 * (Ci1 * mu1 + Ci2 * mu2)
 
-n = length(in);
-
-%Ci
 out.Ci = zeros();
-for i=1:n
-  out.Ci += in(i).Ci;
-end
-
-%C
-out.C = inv(out.Ci);
-
-%mu
 out.mu = zeros();
-for i=1:n
+
+for i = 1:length(in);
+  out.Ci += in(i).Ci;
   out.mu += in(i).Ci * in(i).mu;
 end
+out.C = inv(out.Ci);
 out.mu = out.C * out.mu;
 
