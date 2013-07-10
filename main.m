4158250d36504af4b492b993e564fae4282bfd13
triszalo <szaloki.david@aut.bme.hu>
2013-07-10 15:20:06 +0200

    main calculates and draws ellipses for more positions

--- main.m
+++ main.m
@@ -16,15 +16,31 @@ axis([-10 200 -100 100], "equal");
 
 
 cam(1) = CreateCamera();
+cam(1) = CreateCamera(-pi/4, [10;50]);
 cam(2) = CreateCamera(pi/4, [10;-50]);
 DrawCamera(cam)
 
-X = [75;6];
 
-Ci_mu = CalculateCovariance(cam, X);
-C = CombineGaussians(Ci_mu).C;
-h = my_2D_error_ellipse(C, X, 'conf', 0.95);
+%the columns of X are the position vectors
+%X = [75 60 65 ;
+%      6  0  0 ] ;
 
+[gX,gY] = meshgrid(40:2:80, -10:2:10);
+gXs = prod(size(gX));
+gYs = prod(size(gY));
+X = [ reshape(gX, 1, gXs) ;
+      reshape(gY, 1, gYs) ] ;
+
+for i=1:size(X,2)
+  Xt = X(:,i);
+  Ci_mu = CalculateCovariance(cam, Xt);
+  Comb = CombineGaussians(Ci_mu);
+  Ct = Comb.C;
+  Cit = Comb.Ci;
+  if all(eig(Cit) > 1e-10) && all(eig(Ct) > 1e-10)
+    h = my_2D_error_ellipse(Ct, Xt, 'conf', 0.95);
+  end
+end
 
 hold off
 
