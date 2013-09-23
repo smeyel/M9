21f56cf56a5e66ab88e03fe9733c1b969bc54b1d
Dávid Szalóki <triszaloki@gmail.com>
2013-09-23 15:48:15 +0200

    main2: for loop removed

--- main2.m
+++ main2.m
@@ -15,26 +15,15 @@ Gr = 0;
 Hr = 1000;
 
 
-%preallocation
-gW = zeros(size(gX,1), ...
-           size(gX,2));
 
-%for every point in grid
-for i=1:size(gX,1)
-  for j=1:size(gX,2)
+t2 = gX.^2 + gY.^2;
+K4 = (E-F)*(Gr-Hr) + Gr*Hr;
+K2 = E*Hr + F*Gr;
+K0 = E*F;
 
-    x = gX(i,j);
-    y = gY(i,j);
-
-    t2 = x^2+y^2;
-    K4 = (E-F)*(Gr-Hr) + Gr*Hr;
-    K2 = E*Hr + F*Gr;
-    K0 = E*F;
-
-    gW(i,j) = y^2/t2^2*K4 + 1/t2*K2 + K0;
-
-  end
-end
+gW = gY.^2 ./ t2.^2 * K4 + ...
+     1 ./ t2 * K2 + ...
+     K0;
 
 
 
