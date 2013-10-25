f2b7b391886559d8752c4cca3722291e54f7f135
Dávid Szalóki <triszaloki@gmail.com>
2013-10-25 17:02:44 +0200

    The max locations are calculated on every curve segments; the global optimum is found by a solver on a greater area.

--- main3.m
+++ main3.m
@@ -2,9 +2,14 @@ function [] = main3()
 
 % file for trying the actual problem
 % There is given a camera configuration.
-% There is given constraints for the new camera placement.
-% Finding the optimal solution with fmincon.
-% Plot the max-line from the partial derivatives.
+% There is given constraints for the new camera placement. It is built from
+% one-parametrized curves.
+% The contour of the objective function of the new camera placement is
+% plotted.
+% The max locations are calculated on every curve segment and plotted by
+% red stars.
+% The global maximum on the curves are plotted with green star.
+% The global optimum found by the solver is plotted with a black star.
 
 
 %% preparation
@@ -22,7 +27,7 @@ global useFoV;
 useFoV=false;
 
 
-%% calc
+%% more preparations
 
 
 displayArea = [0 150 -60 70];
@@ -37,37 +42,69 @@ gY = 0;
 gsCovRes = CalculateResultingCovariance(cam, [gX;gY]);
 
 
-maxSlope = getMaxSlope(gsCovRes, cam(1).e_pixel, cam(1).f_pixel);
-maxRayFromSlope = createRay([gX gY], [gX+1 gY+maxSlope]);
+%% curves as constraints
 
+syms a
+
+f{1} = [85;-50]         + a * [0 ; 10];
+f{2} = subs(f{1}, a, 1) + a * [5 ; 10];
+f{3} = subs(f{2}, a, 1) + a * [-5 ; 10];
+f{4} = subs(f{3}, a, 1) + a * [0 ; 20];
+f{5} = subs(f{4}, a, 1) + a * [10 ; 10];
+f{6} = subs(f{5}, a, 1) + a * [0 ; 40];
+f{7} = subs(f{6}, a, 1) + a * [5 ; 0];
+f{8} = subs(f{7}, a, 1) + a * [0 ; -100];
+f{9} = subs(f{8}, a, 1) + a * [-15 ; 0];
+
+
+%% objective function values for the contour plot
 
 [nX,nY] = meshgrid(85:1:100, -50:1:50);
-
 nsCovRes = arrayfun(@(nx,ny) CalculateResultingCovariance([cam,CreateCamera(GetAlpha2D(gX-nx, gY-ny),[nx;ny])], [gX;gY]), ...
     nX, nY);
 nW = arrayfun(@(covres) det(covres.Ci), nsCovRes);
 
 
-minX = min(min(nX));
-maxX = max(max(nX));
-minY = min(min(nY));
-maxY = max(max(nY));
-[x,fval,exitflag,output,lambda,grad,hessian] = fmincon(@(x) myfunc(cam, gX, gY, x), ...
-    [minX;minY], ...
+%% call the solver on the grid
+
+% min or max on the new grid
+minXn = min(min(nX));
+maxXn = max(max(nX));
+minYn = min(min(nY));
+maxYn = max(max(nY));
+[x,fval,exitflag,output,lambda,grad,hessian] = fmincon(@(x) objfunc(cam, gX, gY, x), ...
+    [minXn;minYn], ...
     [], [], ...
     [], [], ...
-    [minX;minY], [maxX;maxY]);
-
-nArea = [minX minY ; ...
-         minX maxY ; ...
-         maxX maxY ; ...
-         maxX minY];
-
-maxXFromSolver = x;
-maxRayFromSolver = createRay([gX gY], maxXFromSolver');
+    [minXn;minYn], [maxXn;maxYn]);
+maxXFromSolver = x(1);
+maxYFromSolver = x(2);
 
 
-%figure
+%% maxLines from the observed point
+
+maxSlope = getMaxSlope(gsCovRes, cam(1).e_pixel, cam(1).f_pixel);
+maxRayFromSlopeP = createRay([gX gY], [gX+1 gY+maxSlope]);
+maxRayFromSlopeN = createRay([gX gY], [gX+1 gY-maxSlope]);
+
+
+%% the extremal points of the curve segments
+
+[maxXc maxYc maxFc] = arrayfun(@(func) getMaxForLine(gsCovRes, cam(1).e_pixel, cam(1).f_pixel, func), f, 'UniformOutput', false);
+maxXs = cell2mat(maxXc);
+maxYs = cell2mat(maxYc);
+maxFs = cell2mat(maxFc);
+
+
+%% the global optimum
+
+[maxF maxF_index] = max(maxFs);
+maxX = maxXs(maxF_index);
+maxY = maxYs(maxF_index);
+
+
+%% figure
+
 fig_my = figure; clf;
 hold on
 contour(nX,nY,nW, 60);
@@ -76,31 +113,78 @@ xlabel('x')
 ylabel('y', 'rotation', 0)
 
 arrayfun(@(c) DrawCamera(c), cam);
-drawPolygon(nArea, 'k')
-
 my_2D_error_ellipse(100*gsCovRes.C, [gX;gY], 'conf', 0.95);
 
-plot(maxXFromSolver(1), maxXFromSolver(2), 'r*')
-drawRay(maxRayFromSolver);
+cellfun(@(func) plotCurve(func), f);
+plot(maxXs, maxYs, 'r*');
+plot(maxX, maxY, 'g*');
 
-drawRay(maxRayFromSlope);
+drawRay(maxRayFromSlopeP);
+drawRay(maxRayFromSlopeN);
+
+
+plot(maxXFromSolver, maxYFromSolver, 'k*')
 
 hold off
 
 
 
-function f = myfunc(cam, gX, gY, x)
+function plotCurve(func)
+% plot one symbolic curve
+syms a
+b = 0:0.1:1;
+Xc = arrayfun(@(b) subs(func, a, b), b, 'UniformOutput', false);
+Xs = cell2mat(Xc);
+plot(Xs(1,:), Xs(2,:));
 
+
+function [maxX maxY maxF] = getMaxForLine(covres, e, f, func)
+% calculate the extremal point of the objective function on one curve
+
+[A B C] = getConsts(covres, e, f);
+
+Dx = covres.mu(1);
+Dy = covres.mu(2);
+
+syms a
+
+xy0 = subs(func, a, 0);
+xy1 = subs(func, a, 1);
+x0 = xy0(1);
+y0 = xy0(2);
+x1 = xy1(1);
+y1 = xy1(2);
+dx = x1-x0;
+dy = y1-y0;
+
+x=x0+a*dx - Dx;
+y=y0+a*dy - Dy;
+d2=x^2+y^2;
+f=y^2/d2^2*A+1/d2*B+C;
+F=diff(f);
+sols = subs(solve(F==0));
+sols01 = sols(sols==real(sols) & 0 < sols & sols < 1);
+
+as = [0;sols01;1];
+fs=subs(f, a, as);
+[maxF maxA_index] = max(fs);
+maxA = as(maxA_index);
+
+maxX = subs(x, a, maxA) + Dx;
+maxY = subs(y, a, maxA) + Dy;
+
+
+function f = objfunc(cam, gX, gY, x)
+% objective function, it has to be minimized
 nx = x(1);
 ny = x(2);
-
 nsCovRes = CalculateResultingCovariance([cam,CreateCamera(GetAlpha2D(gX-nx, gY-ny),[nx;ny])], [gX;gY]);
 nW = det(nsCovRes.Ci);
-
 f = -nW;
 
 
-function slope = getMaxSlope(covres, e, f)
+function [A B C] = getConsts(covres, e, f)
+% calculate the constant values A, B and C
 % e, f: error and focal length of the new camera, same unit
 [v,d] = eig(covres.Ci);
 if(d(1,1)<d(2,2))
@@ -113,4 +197,9 @@ end
 A = (s2-s1) * (e/f)^(-2);
 B = s1 * (e/f)^(-2);
 C = s1 * s2;
+
+
+function slope = getMaxSlope(covres, e, f)
+% calculate the slope of the maxLine from the observed point
+[A B C] = getConsts(covres, e, f);
 slope = sqrt((A-B)/(A+B));
