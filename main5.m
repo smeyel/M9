e6054f0a7bd8601e9d3503f54268073afb231cb5
Dávid Szalóki <triszaloki@gmail.com>
2013-12-06 14:59:01 +0100

    FIXED: rotation matrices; little renaming

--- main5.m
+++ main5.m
@@ -35,13 +35,15 @@ ylabel('z', 'rotation', 0)
 saveas(fig_contour_3d, 'figures/contour_3d_1.eps', 'epsc')
 
 
-function W = myfunc(v1, v2)
+function W = myfunc(d1, d2)
 
 % parametric plane
 p = [0;4;0];
-p1 = [1;0;0];
-p2 = [0;0;1];
-xyz = p + v1*p1 + v2*p2;
+v1 = [0;0;1];
+v2 = [1;0;0];
+v1 = v1 / norm(v1);
+v2 = v2 / norm(v2);
+xyz = p + d1*v1 + d2*v2;
 x = xyz(1);
 y = xyz(2);
 z = xyz(3);
@@ -49,7 +51,7 @@ z = xyz(3);
 % actual values (s1,s2,e,f) get from main3: gsCovRes.Ci
 s1 = 20.4800;
 s2 = 327.6800;
-s3 = 1.4*s2;
+s3 = 10*s2;
 e = 1;
 f = 480;
 K4 = (e/f)^(-2);
@@ -58,14 +60,16 @@ K5 = K4 / 2;
 d = sqrt(x^2+y^2+z^2);
 a = GetAlpha2D(x,y);
 b = asin(z/d);
+c = 0;
 
 s4 = K4 * 1/d^2;
 s5 = K5 * 1/d^2;
 S1 = [s1 0 0 ; 0 s2 0 ; 0 0 s3];
 S2 = [0 0 0 ; 0 s4 0 ; 0 0 s5];
-Ra = [cos(a) -sin(a) 0 ; 0 0 1 ; sin(a) cos(a) 0];
-Rb = [cos(b) 0 sin(b) ; 0 1 0 ; -sin(b) 0 cos(b)];
-Se = S1 + Rb'*Ra'*S2*Ra*Rb;
+Rx = [1 0 0 ; 0 cos(c) -sin(c) ; 0 sin(c) cos(c)];
+Ry = [cos(b) 0 sin(b) ; 0 1 0 ; -sin(b) 0 cos(b)];
+Rz = [cos(a) -sin(a) 0 ; sin(a) cos(a) 0 ; 0 0 1];
+Se = S1 + Rz*Ry*Rx*S2*Rx'*Ry'*Rz';
 W = min(eig(Se));
 
 
