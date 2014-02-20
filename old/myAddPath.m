4e6555b9b9ebc638c30ea66fde1ddbe7afc7c2e3
Dávid Szalóki <triszaloki@gmail.com>
2014-02-20 11:08:13 +0100

    some files moved to folder: old

---
+++ old/myAddPath.m
@@ -0,0 +1,14 @@
+
+%add library paths
+
+addpath('../');
+
+addpath('../library/error_ellipse/');
+
+addpath('../library/geom2d/geom2d/');
+addpath('../library/geom2d/polygons2d/');
+addpath('../library/geom2d/polynomialCurves2d/');
+
+addpath('../library/geom3d/geom3d/');
+addpath('../library/geom3d/meshes3d/');
+
