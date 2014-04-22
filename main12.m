33832c0a4bd084d5175004850226dc356d41c10f
Dávid Szalóki <triszaloki@gmail.com>
2014-04-22 18:30:31 +0200

    [main12] constraints as cell array of polygons; calc_opt_plane() in 2D written, TODO: 3D; TODO:  - eigenvectors are not parallel to the axes  - object is not placed at the origin

--- main12.m
+++ main12.m
@@ -3,6 +3,8 @@ function [] = main12()
 % two camera observing the origin
 % a third camera is placed
 % fmincon in 2D
+% calc_opt_plane: object in origin and eigenvalues are parallel to the axes
+%   2D OK, in the 3D case correction needed, TODO
 
 close all
 clear
@@ -11,56 +13,31 @@ clc
 global useDetectAngle
 useDetectAngle = false;
 
-global firstCall
-
 myAddPath
 
 cams = {CreateCamera('oripos', [0;1], [-1000; 500]), ...
         CreateCamera('oripos', [0;1], [-1000; -500])};
     
-constraints = [ -500, 500 ; ...
-                -500, 2000 ; ...
-                 500, 2000 ; ...
-                 500, 500 ];
-constraints = [constraints ; constraints(1,:)];
-
-F = (cams{1}.e/cams{1}.fx)^(-2);
-ee =  eig(calc_Ciw(cams, zeros(cams{1}.dim,1)));
-E = ee(2)-ee(1);
-D = ee(2)+ee(1);
-dist = sqrt(F/E);
+polygon{1} = [ -500, 500 ; ...
+                   -500, 2000 ; ...
+                    500, 2000 ; ...
+                    500, 500 ];
+polygon{2} = [ -400, 400 ; ...
+                   -400, 1000 ; ...
+                    400, 1000 ; ...
+                    400, 400 ];
+polygon{3} = [ -600, 1500 ; ...
+                   1000, 500 ; ...
+                   -400, 600 ];
+polygon = cellfun(@(p) [p;p(1,:)], polygon, 'uni', false);
 
             
 hold on
 DrawCamera(cams)
-plot(constraints(:,1),constraints(:,2))
-
-start = [500; 1500];
-lb = [0;10];
-ub = [5000;5000];
-
-firstCall = true;
-xopt = fmincon(...
-    @(v) -myfun(cams,v), ... %fun
-    start, ... %x0
-    [], [], ... %A, b
-    [], [], ... %Aeq, beq
-    lb, ... %lb
-    ub, ... %ub
-    [], ... %nonlcon
-    optimset('OutputFcn', @outputfun)); %options
-plot(xopt(1), xopt(2), 'r*')
+cellfun(@(p) plot(p(:,1),p(:,2)), polygon);
 hold off
 
-xopt
-myfun(cams, xopt)
 
-
-if inpolygon(xopt(1), xopt(2), constraints(:,1),constraints(:,2))
-    disp('IN')
-else
-    disp('OUT')
-end
 
 
 return
@@ -84,4 +61,50 @@ firstCall = false;
 plot(x(1), x(2), 'g*')
 pre = x;
 stop = 0;
+
+
+function [xopt in] = calc_opt_plane(cams, polygon)
+
+if cams{1}.dim == 2
+    F = (cams{1}.e/cams{1}.fx)^(-2);
+    ee =  eig(calc_Ciw(cams, zeros(cams{1}.dim,1)));
+    E = ee(2)-ee(1);
+    D = ee(2)+ee(1);
+    dist = sqrt(F/E);
+    xmax = [0;dist];
+
+    if inpolygon(xmax(1), xmax(2), polygon(:,1),polygon(:,2))
+        xopt = xmax;
+        in = true;
+    else
+        [XI,YI] = polyxpoly(polygon(:,1),polygon(:,2),[0;xmax(1)],[0;xmax(2)]);
+        if isempty(XI)
+            xopt = xmax;
+            in = false;
+        else
+            % the first intersection is selected
+            xopt = [XI';YI'];
+            xopt = xopt(:,1);
+            in = true;
+        end
+    end
+
+else
+
+    % TODO: only moved, correction needed
+    start = [500; 1500];
+    lb = [0;10];
+    xopt = fmincon(...
+        @(v) -myfun(cams,v), ... %fun
+        start, ... %x0
+        [], [], ... %A, b
+        [], [], ... %Aeq, beq
+        lb, ... %lb
+        [], ... %ub
+        [], ... %nonlcon
+        optimset('OutputFcn', @outputfun)); %options
+    plot(xopt(1), xopt(2), 'r*')
+    in = inpolygon(xopt(1), xopt(2), polygon(:,1),polygon(:,2));
+
+end
 
