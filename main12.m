4de8c5ef5d940e6616511fbbf488eaff81687284
Dávid Szalóki <triszaloki@gmail.com>
2014-04-25 18:11:08 +0200

    [main12] iterative optimal placement first try

--- main12.m
+++ main12.m
@@ -13,33 +13,50 @@ useDetectAngle = false;
 
 myAddPath
 
-cams = {CreateCamera('pos', [-1000; 500; 0]), ...
-        CreateCamera('pos', [-1000; -500; 0])};
-    
-polygon{1} = [ -500, 1500, 100 ; ...
-                   -500, 2000, 100 ; ...
-                    500, 1600, 100 ; ...
-                    0, 1000, 100 ];
-polygon = cellfun(@(p) [p;p(1,:)], polygon, 'uni', false);
 
-objX = [0;0;0];
+cams = {CreateCamera('pos', [   500;  1500 ]), ...
+        CreateCamera('pos', [ -1000;   500 ]), ...
+        CreateCamera('pos', [ -1000;  -500 ])};
+
+polygon{1} = [  -500,  1500 ; ...
+                -500,  2000 ; ...
+                -300,  2000 ; ...
+                 600,  1500 ; ...
+                   0,  1100 ];
+polygon{2} = [  -400,  1200 ; ...
+                -700,  1600 ; ...
+               -1100,   500 ; ...
+                -300,  -300 ];
+polygon{3} = [ -1200,   200 ; ...
+                -700,  -700 ; ...
+               -1000,  -600 ];
+polygon = cellfun(@(p) [p;p(1,:)], polygon, 'uni', false);
+num = numel(cams);
+out = arrayfun(@(i) inpolygon(cams{i}.pos(1), cams{i}.pos(2), polygon{i}(:,1),polygon{i}(:,2)), 1:num);
+if ~all(out)
+    error(['camera number i is not placed in polygon with number i (i = ' num2str(find(~out)) ')'])
+end
+
+objX = [0;0];
 
             
 hold on
+axis('equal')
 DrawCamera(cams)
 
-if cams{1}.dim == 3
-    cellfun(@(p) plot3(p(:,1),p(:,2),p(:,3)), polygon); % plot all polygon
-    xopts = cellfun(@(p) calc_opt_polygon(cams, p, objX), polygon, 'uni', false);
-    xopts = cell2mat(xopts)';
-    plot3(xopts(:,1), xopts(:,2), xopts(:,3), 'r*') % plot all xopts with red
-    plot3(objX(1), objX(2), objX(3), 'g*') % plot objX
-else
-    cellfun(@(p) plot(p(:,1),p(:,2)), polygon); % plot all polygon
-    xopts = cellfun(@(p) calc_opt_polygon(cams, p, objX), polygon, 'uni', false);
-    xopts = cell2mat(xopts)';
-    plot(xopts(:,1), xopts(:,2), 'r*') % plot all xopts with red
-    plot(objX(1), objX(2), 'g*') % plot objX
+cellfun(@(p) plot(p(:,1),p(:,2)), polygon); % plot all polygon
+plot(objX(1), objX(2), 'g*') % plot objX
+
+for i=1:6
+    j = mod(i,num);
+    if(j==0)
+        j=3;
+    end
+    cams_i = cams;
+    cams_i(j) = [];
+    xopt = calc_opt_polygon(cams_i, polygon{j}, objX);
+    cams{j} = CreateCamera('pos', xopt);
+    plot(xopt(1), xopt(2), 'r*') % plot all xopts with red
 end
 
 hold off
