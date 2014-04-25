1d1e5608e7f712a6daaa72d6f68871e7c02fd888
Dávid Szalóki <triszaloki@gmail.com>
2014-04-25 19:02:26 +0200

    [main12] 3 cameras, iterative placement, the optimum is not found

--- main12.m
+++ main12.m
@@ -3,6 +3,7 @@ function [] = main12()
 % two camera observing the oobject at objX
 % a third camera is placed
 % calc_opt_plane applied
+% iterative placement of the 3 cameras, the optimum is not found
 
 close all
 clear
@@ -14,21 +15,21 @@ useDetectAngle = false;
 myAddPath
 
 
-cams = {CreateCamera('pos', [   500;  1500 ]), ...
-        CreateCamera('pos', [ -1000;   500 ]), ...
-        CreateCamera('pos', [ -1000;  -500 ])};
+cams = {CreateCamera('pos', [  900;    50 ]), ...
+        CreateCamera('pos', [ -1000;   400 ]), ...
+        CreateCamera('pos', [  -900;  -500 ])};
 
-polygon{1} = [  -500,  1500 ; ...
-                -500,  2000 ; ...
-                -300,  2000 ; ...
-                 600,  1500 ; ...
-                   0,  1100 ];
-polygon{2} = [  -400,  1200 ; ...
-                -700,  1600 ; ...
+polygon{1} = [   500,     0 ; ...
+                700,   500 ; ...
+                1100,  -100 ; ...
+                 600,  -100 ; ...
+                 600,  -500 ];
+polygon{2} = [  0, 500/cos(pi/6) ; ...
+                -500/cos(pi/3)+200, 0+200*tan(pi/6) ; ...
                -1100,   500 ; ...
-                -300,  -300 ];
-polygon{3} = [ -1200,   200 ; ...
-                -700,  -700 ; ...
+                -800,   300 ];
+polygon{3} = [  -500/cos(pi/3), 0 ; ...
+                500*cos(-2*pi/3),  500*sin(-2*pi/3) ; ...
                -1000,  -600 ];
 polygon = cellfun(@(p) [p;p(1,:)], polygon, 'uni', false);
 num = numel(cams);
@@ -47,7 +48,8 @@ DrawCamera(cams)
 cellfun(@(p) plot(p(:,1),p(:,2)), polygon); % plot all polygon
 plot(objX(1), objX(2), 'g*') % plot objX
 
-for i=1:6
+
+for i=1:15
     j = mod(i,num);
     if(j==0)
         j=3;
@@ -59,6 +61,21 @@ for i=1:6
     plot(xopt(1), xopt(2), 'r*') % plot all xopts with red
 end
 
+
+camo = {CreateCamera('pos', [     500;   0 ]), ...
+        CreateCamera('pos', [500*cos(2*pi/3); 500*sin(2*pi/3)]), ...
+        CreateCamera('pos', [500*cos(-2*pi/3); 500*sin(-2*pi/3)])};
+DrawCamera(camo, 'm')
+Ci = calc_Ciw(cams, objX);
+C = pinv(Ci);
+min(eig(Ci))
+Cio = calc_Ciw(camo, objX);
+Co = pinv(Cio);
+min(eig(Cio))
+
+cd 'old'
+my_2D_error_ellipse(1000*C,  objX, 'conf', 0.95, 'style', 'r');
+my_2D_error_ellipse(1000*Co, objX, 'conf', 0.95);
+cd '..'
+
 hold off
-
-
