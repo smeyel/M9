9a5d97a22e6138a1a1edf0bfe8af1257820858ff
Dávid Szalóki <triszaloki@gmail.com>
2014-04-25 15:21:31 +0200

    [main12] plots for 3D and 2D as well

--- main12.m
+++ main12.m
@@ -28,15 +28,19 @@ objX = [0;0;0];
 hold on
 DrawCamera(cams)
 
-%cellfun(@(p) plot(p(:,1),p(:,2)), polygon); % plot all polygon
-cellfun(@(p) plot3(p(:,1),p(:,2),p(:,3)), polygon); % plot all polygon
-xopts = cellfun(@(p) calc_opt_polygon(cams, p, objX), polygon, 'uni', false);
-xopts = cell2mat(xopts)';
-%plot(xopts(:,1), xopts(:,2), 'r*') % plot all xopts with red
-plot3(xopts(:,1), xopts(:,2), xopts(:,3), 'r*') % plot all xopts with red
-
-%plot(objX(1), objX(2), 'g*') % plot objX
-plot3(objX(1), objX(2), objX(3), 'g*') % plot objX
+if cams{1}.dim == 3
+    cellfun(@(p) plot3(p(:,1),p(:,2),p(:,3)), polygon); % plot all polygon
+    xopts = cellfun(@(p) calc_opt_polygon(cams, p, objX), polygon, 'uni', false);
+    xopts = cell2mat(xopts)';
+    plot3(xopts(:,1), xopts(:,2), xopts(:,3), 'r*') % plot all xopts with red
+    plot3(objX(1), objX(2), objX(3), 'g*') % plot objX
+else
+    cellfun(@(p) plot(p(:,1),p(:,2)), polygon); % plot all polygon
+    xopts = cellfun(@(p) calc_opt_polygon(cams, p, objX), polygon, 'uni', false);
+    xopts = cell2mat(xopts)';
+    plot(xopts(:,1), xopts(:,2), 'r*') % plot all xopts with red
+    plot(objX(1), objX(2), 'g*') % plot objX
+end
 
 hold off
 
