6a5346347334acbff4722883d9f92a3a6adaee1d
Dávid Szalóki <triszaloki@gmail.com>
2014-04-27 10:54:51 +0200

    [main12_3] outputfun() only stores the positions and measure values through the iterations, plot is performed after all calculations

--- main12_3.m
+++ main12_3.m
@@ -25,16 +25,25 @@ segment{2} = [px(2)+d(2,1)*nx(2), py(2)+d(2,1)*ny(2), px(2)+d(2,2)*nx(2), py(2)+
 segment{3} = [px(3)+d(3,1)*nx(3), py(3)+d(3,1)*ny(3), px(3)+d(3,2)*nx(3), py(3)+d(3,2)*ny(3)];
 
 
-figure
+global xopts
+global qs
+xopts = {};
+qs = [];
+
+calc_opt_line_ori(segment);
+
+figure(1)
 hold on
 cellfun(@(s) plot([s(1);s(3)],[s(2);s(4)]), segment); % plot all segment
-xopt = calc_opt_line_ori(segment);
-plot(xopt(:,1), xopt(:,2), 'b*');
+cellfun(@(x) plot(x(1,1), x(1,2), 'g*'), xopts) % plot cam 1 iteration positions
+cellfun(@(x) plot(x(2,1), x(2,2), 'r*'), xopts) % plot cam 2 iteration positions
+cellfun(@(x) plot(x(3,1), x(3,2), 'm*'), xopts) % plot cam 3 iteration positions
+plot(xopts{end}(:,1), xopts{end}(:,2), 'b*'); %plot the optimal positions of all cameras
 axis('equal')
 hold off
 
-xopt
-myfun(xopt)
+figure(2)
+plot(qs, 'k*')
 
 
 function [xopt q] = calc_opt_line_ori(segments)
@@ -68,18 +77,22 @@ function stop = outputfun(t, optimValues, state)
 % plots the camera positions
 
 global segment
-x0 = cellfun(@(s) s(1), segment)';
-y0 = cellfun(@(s) s(2), segment)';
-x1 = cellfun(@(s) s(3), segment)';
-y1 = cellfun(@(s) s(4), segment)';
-dx = x1-x0;
-dy = y1-y0;
+global xopts
+global qs
 
-x = [x0+t.*dx,y0+t.*dy];
+if strcmp(state, 'iter')
+    x0 = cellfun(@(s) s(1), segment)';
+    y0 = cellfun(@(s) s(2), segment)';
+    x1 = cellfun(@(s) s(3), segment)';
+    y1 = cellfun(@(s) s(4), segment)';
+    dx = x1-x0;
+    dy = y1-y0;
 
-plot(x(1,1), x(1,2), 'g*')
-plot(x(2,1), x(2,2), 'r*')
-plot(x(3,1), x(3,2), 'm*')
+    index = optimValues.iteration+1;
+    x = [x0+t.*dx,y0+t.*dy];
+    xopts{index} = x;
+    qs(index) = myfun(x);
+end
 stop = 0;
 
 
