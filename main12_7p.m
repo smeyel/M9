93b293489d861fb071b35c2c54143cce3dc2de54
Dávid Szalóki <triszaloki@gmail.com>
2014-07-10 18:43:11 +0200

    [main12_7p] if the observed point is outside, the optimal solution is transformed: b2=pi-b2; the objective function is also modified

--- main12_7p.m
+++ main12_7p.m
@@ -9,6 +9,7 @@ clc
 
 
 eig_or_det = false;
+between_or_outside = false;
 
 m1 = 2;
 m2 = 3;
@@ -22,14 +23,24 @@ d12 = d1^2;
 d22 = d2^2;
 
 if eig_or_det
-    f = 1/d12 + 1/d22 - sqrt(1/d12^2 + 1/d22^2 + 2/(d12*d22) * cos(2*(g1+g2)));
     b1 = atan(m1/m2);
     b2 = atan(m2/m1);
+    if between_or_outside
+        f = 1/d12 + 1/d22 - sqrt(1/d12^2 + 1/d22^2 + 2/(d12*d22) * cos(2*(g1+g2)));
+    else
+        f = 1/d12 + 1/d22 - sqrt(1/d12^2 + 1/d22^2 + 2/(d12*d22) * cos(2*(g1-g2)));
+        b2 = pi - b2;
+    end
 else
-    f = sin(g1+g2)^2 / (d12 * d22);
-    b = (1/3 * [2 -1 ; -1 2] * [pi ; pi]);
+    b = linsolve([2,1;1,2],[pi;pi]);
     b1 = b(1);
     b2 = b(2);
+    if between_or_outside
+        f = sin(g1+g2)^2 / (d12 * d22);
+    else
+        f = sin(g1-g2)^2 / (d12 * d22);
+        b2 = pi - b2;
+    end
 end
 
 
