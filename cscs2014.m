2d9a1c90d5ea833a56bbbda1945180d093344f88
Dávid Szalóki <triszaloki@gmail.com>
2014-08-04 19:28:33 +0200

    first version of cscs2014 plots

---
+++ cscs2014.m
@@ -0,0 +1,292 @@
+%function [] = cscs2014()
+
+% todo
+
+
+%% preparations
+
+close all
+clear
+clc
+
+myAddPath
+addpath('old/');
+
+
+cams{1} = CreateCamera('pos', [0;-400]);
+cams{2} = CreateCamera('pos', [0;400]);
+
+K = cams{1}.fx / cams{1}.e;
+
+style = hgexport('factorystyle');
+style.Color = 'gray';
+
+
+
+%% merge
+
+objX = [700;150];
+newSig = 0.015;
+
+Ci1 = calc_Ciw(cams(1), objX);
+[V D] = eig(Ci1);
+D(1,1) = newSig;
+Ci1 = V*D*V';
+C1 = pinv(Ci1);
+
+Ci2 = calc_Ciw(cams(2), objX);
+[V D] = eig(Ci2);
+D(1,1) = newSig;
+Ci2 = V*D*V';
+C2 = pinv(Ci2);
+
+Ci = Ci1 + Ci2;
+C = pinv(Ci);
+
+fig_merge = figure;
+hold on
+plot(objX(1), objX(2), 'kx')
+DrawCamera(cams{1}, 'k');
+DrawCamera(cams{2}, 'k');
+
+my_2D_error_ellipse(1000*C,  objX, 'conf', 0.95, 'style', 'k');
+my_2D_error_ellipse(1000*C1,  objX, 'conf', 0.95, 'style', 'k');
+my_2D_error_ellipse(1000*C2,  objX, 'conf', 0.95, 'style', 'k');
+
+axis equal
+xlim([-100 1300])
+ylim([-550 550])
+xlabel('x')
+ylabel('y', 'rotation', 0)
+hold off
+
+hgexport(fig_merge,'figures/cscs2014/merge.eps',style);
+
+
+
+%% ellipses
+
+[gX,gY] = meshgrid(100:100:1200, -500:100:500);
+
+fig_ellipses = figure;
+hold on
+
+arrayfun(@(c) DrawCamera(c, 'k'), cams);
+arrayfun(@(gx, gy) my_2D_error_ellipse(30*pinv(calc_Ciw(cams, [gx;gy])), [gx;gy], 'conf', 0.95, 'style', 'k'), ...
+    gX, gY);
+
+axis equal
+xlim([-100 1300])
+ylim([-550 550])
+xlabel('x')
+ylabel('y', 'rotation', 0)
+hold off
+
+hgexport(fig_ellipses,'figures/cscs2014/ellipses.eps',style);
+
+
+
+%% wellness plots
+
+syms x y real
+
+x1 = cams{1}.pos(1) - x;
+x2 = cams{2}.pos(1) - x;
+y1 = cams{1}.pos(2) - y;
+y2 = cams{2}.pos(2) - y;
+
+d1 = sqrt(x1^2+y1^2);
+d2 = sqrt(x2^2+y2^2);
+g1=atan(y1/x1);
+g2=atan(y2/x2);
+
+fe = 1/d1^2 + 1/d2^2 - sqrt(1/d1^4 + 1/d2^4 + 2/(d1^2*d2^2)*cos(2*(g1-g2)));
+fd = 1/d1^2 * 1/d2^2 * sin(g1-g2)^2;
+ft = 1/d1^2 + 1/d2^2;
+
+fe = K^2 * fe;
+fd = K^2 * fd;
+ft = K^2 * ft;
+
+fig_fe = figure;
+ezsurf(fe, [  0 1000 -1000 1000])
+xlim([0 1000])
+ylim([-1000 1000])
+zlim([0 5])
+xlabel('x')
+ylabel('y', 'rotation', 0)
+zlabel('z', 'rotation', 0)
+title('fe')
+
+fig_fd = figure;
+ezsurf(fd, [100 1000 -1000 1000])
+xlim([0 1000])
+ylim([-1000 1000])
+zlim([0 1.5e-4])
+xlabel('x')
+ylabel('y', 'rotation', 0)
+zlabel('z', 'rotation', 0)
+title('fd')
+
+fig_ft = figure;
+ezsurf(ft, [100 1000 -1000 1000])
+xlim([0 1000])
+ylim([-1000 1000])
+zlim([0 90])
+xlabel('x')
+ylabel('y', 'rotation', 0)
+zlabel('z', 'rotation', 0)
+title('ft')
+
+hgexport(fig_fe,'figures/cscs2014/fe.eps',style);
+hgexport(fig_fd,'figures/cscs2014/fd.eps',style);
+hgexport(fig_ft,'figures/cscs2014/ft.eps',style);
+
+
+
+%% placement
+
+syms g d real
+R_gen = [cos(g) -sin(g) ; sin(g) cos(g)];
+Ci_gen = R_gen * [1/d^2 0 ; 0 0] * R_gen';
+
+
+a1 = 14*pi/180;
+a2 = 10*pi/180;
+
+m = 1;
+
+syms g1 g2 real
+
+d1 = m*sin(a1)/sin(a1+g1);
+d2 = m*sin(a2)/sin(a2-g2);
+
+Ci1 = subs(Ci_gen, {g,d}, {g1,d1});
+Ci2 = subs(Ci_gen, {g,d}, {g2,d2});
+Ci = Ci1+Ci2;
+
+
+fe = 1/d1^2 + 1/d2^2 - sqrt(1/d1^4 + 1/d2^4 + 2/(d1^2*d2^2)*cos(2*(g1-g2)));
+b1e =  atan((1-1/tan(a2)) / (1-1/tan(a1)));
+b2e = -atan((1-1/tan(a1)) / (1-1/tan(a2)));
+
+fd = det(Ci);
+bd = (1/3 * [2 -1 ; -1 2] * [pi-a1 ; pi-a2]);
+b1d =  bd(1);
+b2d = -bd(2);
+
+ft = trace(Ci);
+b1t =  (pi/2 - a1);
+b2t = -(pi/2 - a2);
+
+
+[x y] = pol2cart(b1e, subs(d1,g1,b1e));
+p1e = [x;y];
+[x y] = pol2cart(b2e, subs(d2,g2,b2e));
+p2e = [x;y];
+
+[x y] = pol2cart(b1d, subs(d1,g1,b1d));
+p1d = [x;y];
+[x y] = pol2cart(b2d, subs(d2,g2,b2d));
+p2d = [x;y];
+
+[x y] = pol2cart(b1t, subs(d1,g1,b1t));
+p1t = [x;y];
+[x y] = pol2cart(b2t, subs(d2,g2,b2t));
+p2t = [x;y];
+
+
+fig_placement = figure;
+hold on
+plot(0, 0, 'k*')
+
+plot([0 2*m],  m*tan(a1)*[1 -1], 'k')
+plot([0 2*m], -m*tan(a2)*[1 -1], 'k')
+
+plot([0 p1e(1)], [0 p1e(2)], 'k--')
+plot([0 p2e(1)], [0 p2e(2)], 'k--')
+plot([0 p1d(1)], [0 p1d(2)], 'k-.')
+plot([0 p2d(1)], [0 p2d(2)], 'k-.')
+plot([0 p1t(1)], [0 p1t(2)], 'k:')
+plot([0 p2t(1)], [0 p2t(2)], 'k:')
+
+plot(p1e(1),p1e(2), 'ks', 'MarkerFaceColor','w')
+plot(p2e(1),p2e(2), 'ks', 'MarkerFaceColor','w')
+plot(p1d(1),p1d(2), 'ko', 'MarkerFaceColor','w')
+plot(p2d(1),p2d(2), 'ko', 'MarkerFaceColor','w')
+plot(p1t(1),p1t(2), 'kv', 'MarkerFaceColor','w')
+plot(p2t(1),p2t(2), 'kv', 'MarkerFaceColor','w')
+
+axis equal
+xlim([-0.1 1.1])
+ylim([-0.2 0.3])
+xlabel('x')
+ylabel('y', 'rotation', 0)
+hold off
+
+hgexport(fig_placement,'figures/cscs2014/placement.eps',style);
+
+
+
+%% objective function plots
+limx = [-a1 ; pi-a1];
+limy = [-(-a2) ; -(pi-a2)];
+min1 = min(limx);
+max1 = max(limx);
+min2 = min(limy);
+max2 = max(limy);
+
+
+fig_cfe = figure;
+set(gcf, 'colormap', gray)
+hold on
+ezcontour(fe, [min1 max1 min2 max2])
+h = ezplot('g1-g2=-pi/2'); set(h,'LineStyle', ':');
+h = ezplot('g1-g2=0'); set(h,'LineStyle', ':');
+h = ezplot('g1-g2=pi/2'); set(h,'LineStyle', ':');
+h = ezplot('g1-g2=pi'); set(h,'LineStyle', ':');
+title('cfe')
+plot(b1e, b2e, 'k*')
+axis equal
+xlim([min1 max1])
+ylim([min2 max2])
+xlabel('x')
+ylabel('y', 'rotation', 0)
+hold off
+
+
+fig_cfd = figure;
+set(gcf, 'colormap', gray)
+hold on
+ezcontour(fd, [min1 max1 min2 max2])
+h = ezplot('g1-g2=-pi/2'); set(h,'LineStyle', ':');
+h = ezplot('g1-g2=0'); set(h,'LineStyle', ':');
+h = ezplot('g1-g2=pi/2'); set(h,'LineStyle', ':');
+h = ezplot('g1-g2=pi'); set(h,'LineStyle', ':');
+title('cfd')
+plot(b1d, b2d, 'k*')
+axis equal
+xlim([min1 max1])
+ylim([min2 max2])
+xlabel('x')
+ylabel('y', 'rotation', 0)
+hold off
+
+
+fig_cft = figure;
+set(gcf, 'colormap', gray)
+hold on
+ezcontour(ft, [min1 max1 min2 max2])
+title('cft')
+plot(b1t, b2t, 'k*')
+axis equal
+xlim([min1 max1])
+ylim([min2 max2])
+xlabel('x')
+ylabel('y', 'rotation', 0)
+hold off
+
+hgexport(fig_cfe,'figures/cscs2014/cfe.eps',style);
+hgexport(fig_cfd,'figures/cscs2014/cfd.eps',style);
+hgexport(fig_cft,'figures/cscs2014/cft.eps',style);
+
